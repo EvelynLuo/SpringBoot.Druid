8e1608417fbdb64bc0a593339341f4bd04ae2fdf
Evelyn <luo.e@yahoo.com>
2020-06-29 17:02:20 +0800

    localhost:8080 error

---
+++ src/main/java/luoe/demo/controller/EmailController.java
@@ -0,0 +1,54 @@
+package luoe.demo.controller;
+
+import luoe.demo.entity.Email;
+import luoe.demo.service.EmailService;
+import luoe.demo.utils.PageUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * @Data 2020/6/29 14:19
+ * @Author ruary
+ * @Version 1.0
+ * @Describe
+ **/
+@Controller
+@RequestMapping("/Email")
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+
+    /*@RequestMapping("/query")
+    public PageUtils query(HttpServletRequest req) {
+        int page = Integer.parseInt(req.getParameter("page"));
+        int size = Integer.parseInt(req.getParameter("size"));
+        String report_name = req.getParameter("report_name");
+    }
+    @ResponseBody
+    @RequestMapping("/queryAll")
+    public List<Email> queryALL(){
+        return  emailService.queryAll();
+    }
+
+    */
+
+    @ResponseBody
+    @RequestMapping("/save")
+    public int save(Email email){
+        return emailService.save(email);
+    }
+
+    @ResponseBody
+    @RequestMapping("/queryAll")
+    public List<Email> queryALL(){
+        return  emailService.queryAll();
+    }
+
+
+}
